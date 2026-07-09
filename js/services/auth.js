f5d101cd92330086c7890fa84be2b12c15bbe4f4
Wizard Promptor <mcgwara43@gmail.com>
2026-07-09 09:14:02 +0300

    Fix auth page wiring by exporting login/register from js/services/auth.js

---
+++ js/services/auth.js
@@ -0,0 +1,7 @@
+// Barrel module for auth service.
+// login.html and register.html import from: ./js/services/auth.js
+// The canonical auth implementation lives at: ../auth.js
+
+export * from '../auth.js';
+export { default } from '../auth.js';
+
